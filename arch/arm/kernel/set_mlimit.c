977fde8e0d4e324e82d279cf4e19848a76dd1872
Hao-Yu <hh2539@columbia.edu>
2013-11-10 22:14:41 -0500

    Fixed typo in syscall interface

--- arch/arm/kernel/set_mlimit.c
+++ arch/arm/kernel/set_mlimit.c
@@ -1,3 +1,11 @@
+
+#include <linux/linkage.h>
+#include <linux/types.h>
+
+int set_mlimit(uid_t uid, long mem_max){
+	return 1;	
+}
+
 asmlinkage int sys_set_mlimit(uid_t uid, long mem_max){
 	return set_mlimit(uid, mem_max);
 }
