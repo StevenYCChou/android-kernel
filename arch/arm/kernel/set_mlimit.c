b2570858fa28f7f400bbb50055777e3cdf6e04bf
Hao-Yu <hh2539@columbia.edu>
2013-11-16 22:08:22 -0500

    Complete the set_mlimit syacall.

--- arch/arm/kernel/set_mlimit.c
+++ arch/arm/kernel/set_mlimit.c
@@ -1,9 +1,27 @@
 
 #include <linux/linkage.h>
 #include <linux/types.h>
+#include <linux/sched.h>
+#include <linux/errno.h>
 
 int set_mlimit(uid_t uid, long mem_max){
-	return 1;	
+	struct user_struct *user;
+
+	if (mem_max <= 0)
+		return -EINVAL;
+
+	user = find_user(uid);
+	if(user == NULL)
+		return -EINVAL;
+
+	user->mem_quota = mem_max;
+
+	printk("Set_mlimit succeed, the mem_max of the user %lu is %lu.\n", 
+		(unsigned long)uid, (unsigned long)user->mem_quota);
+
+	free_uid(user);
+
+	return 0;	
 }
 
 asmlinkage int sys_set_mlimit(uid_t uid, long mem_max){
