1e7f076e9a28fe961a9cb81e4c93c96ca8e81bc8
Hao-Yu <hh2539@columbia.edu>
2013-11-29 20:07:44 -0500

    Compilable version of cowcopy.c

--- fs/ext4/cowcopy.c
+++ fs/ext4/cowcopy.c
@@ -7,30 +7,42 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <asm/stat.h>
+#include <linux/stat.h>
 #include <linux/unistd.h>
+
+//#include <linux/limits.h>
+//#include <linux/ioctl.h>
+//#include <linux/blk_types.h>
+//#include <linux/types.h>
 
 int ext4_cowcopy(const char __user *src, const char __user *dest);
 asmlinkage int sys_ext4_cowcopy(const char __user *src, const char __user *dest);
-
+extern int vfs_stat(const char __user *, struct kstat *);
 
 asmlinkage int sys_ext4_cowcopy(const char __user *src, const char __user *dest){
-	struct __old_kernel_stat st_buf;
+	//struct __old_kernel_stat st_buf;
+	struct kstat kstat;
 
-    printk ("###### sys_ext4_cowcopy is called\n");
+    printk ("### sys_ext4_cowcopy is called\n");
 
-    if (0 != sys_stat(src, &st_buf)) {
+    if (0 != vfs_stat(src, &kstat)) {
         printk ("Error in stat\n");
         return -1;
     }
 
-    if (!S_ISREG (st_buf.st_mode)) {
-        printk ("%s is not a regular file.\n", src);
-        return -EPERM;
-    }
-    if (S_ISDIR (st_buf.st_mode)) {
-        printk ("%s is a directory.\n", src);
-        return -EPERM;
-    }
+    printk("### stat.ino: %llu \n", kstat.ino);
+    printk("### stat.dev: %llu \n", (unsigned long long)kstat.dev);
+    printk("### stat.mode: %llu \n", (unsigned long long)kstat.mode);
+
+
+    // if (!S_ISREG (st_buf.st_mode)) {
+    //     printk ("%s is not a regular file.\n", src);
+    //     return -EPERM;
+    // }
+    // if (S_ISDIR (st_buf.st_mode)) {
+    //     printk ("%s is a directory.\n", src);
+    //     return -EPERM;
+    // }
 
 	return ext4_cowcopy(src, dest);
 }
